ab1772dc65f4e1b41766f952cba8417858074d45
Soumyodeep Dutta <soumyo.tech.deep@example.com>
2024-02-02 11:38:53 +0530

    All Setup Is completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import {asyncHandler} from "../utils/asyncHandlers.js"
 
 const registerUser = asyncHandler( async (req, res) =>{
      res.status(200).json({
-        massage: "ok"
+        massage: "Starting Your Backend Project"
     })
 })
 
