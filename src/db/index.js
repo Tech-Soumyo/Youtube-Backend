e862a4c90251a51b8a3d39c36569877d06cdf0f2
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-03-07 15:10:04 +0530

    Restart

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,21 @@
 import mongoose, { mongo } from "mongoose";
-import {DB_NAME}  from "../constants.js";
-
+import { DB_NAME } from "../constants.js";
 
 // MONGODB_URI= "mongodb+srv://soumyotechdeep:r0ofCa4jc1oYxzTd@backendproject.0ujrrwb.mongodb.net"
 
-const connectDB = async () =>{
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
 
-        console.log(`\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
-
-    } catch (error) {
-        console.log|("MONGODB connection error ", error);
-        process.exit(1);       
-    }
-}
+    console.log(
+      `\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection error ", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,24 @@
 // require ('dotenv').config({path: './env'})
 
-
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from "./app.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
-
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`*  Server is =running at PORT:  ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("MONGODB connection failed !!!", err);
+  path: "./env",
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`*  Server is =running at PORT:  ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection Failed !!!", err);
+  });
 
 /*
 import express from "express";
